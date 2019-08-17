74af062e350460acc16dff5e5553dbe1e16432f0
Yatin GOYAL <yatin.goyal@amadeus.com>
2019-08-18 00:04:28 +0530

    readme updated and few more features of bs used

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Web Crawler
 
 
+![HighLevelFlow](/doc/Architecture.png)
+
+https://en.wikipedia.org/wiki/Main_Page
+
+https://www.crummy.com/software/BeautifulSoup/bs4/doc/
--- src/python/web_crawler.py
+++ src/python/web_crawler.py
@@ -1,4 +1,4 @@
-from urllib2 import urlopen
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import re
 
@@ -7,10 +7,60 @@ conn = urlopen(url)
 pageData = conn.read()
 conn.close()
 
+print(type(pageData))
+
 try:
     bs = BeautifulSoup(pageData, "lxml")
 except:
     bs = BeautifulSoup(pageData, "html5lib")
+
+print(type(bs))
+print(bs.title)
+print(bs.h1)
+print(bs.find("h1", {"id": "firstHeading"}).string)
+print(bs.find("div", {"id": "siteSub"}).text)
+
+# tag and its name
+tag = bs.h1
+print(tag)
+print(type(tag))
+print(tag.name)
+print("==============================")
+
+# attributes of tag
+print(tag.attrs)
+print(tag['class'])
+print(tag.get('id'))
+
+##We can add, remove, and modify a tag’s name and attributes
+
+# modifying tag type
+tag.name = "div"
+
+print(tag)
+print(tag.name)
+print("==============================")
+
+print(tag.attrs)
+print("==============================")
+
+# modifying a attribute
+tag['id'] = 'yatin'
+print(tag.attrs)
+print("==============================")
+
+# adding a new attribute
+tag['xyz'] = 'abc'
+print(tag.attrs)
+print("==============================")
+
+# deleting an attribute
+del tag['xyz']
+print(tag.attrs)
+print("==============================")
+
+
+#Fetching multiple links and putting in csv
 
 headers = "Title, Link, Value\n"
 filename = "scrapped.csv"
@@ -34,3 +84,6 @@ for link in links:
         continue
 
 f.close()
+
+# Converting the HTML to readable and structured format
+print(bs.prettify())
